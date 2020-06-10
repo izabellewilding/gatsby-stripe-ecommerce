2a127b4a32a91afd29c20b6c4cd65b3350fbeb45
izabelle wilding <izabellewilding@gmail.com>
2020-06-10 12:24:01 +0100

    Swapped Gatsby BG for Img

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,22 +1,12 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
-import BackgroundImage from "gatsby-background-image"
+import Img from "gatsby-image"
 import Button from "@material-ui/core/Button"
 
 const Hero = () => {
-  const { mobileBanner, desktopBanner } = useStaticQuery(
+  const { desktopBanner } = useStaticQuery(
     graphql`
       query {
-        mobileBanner: file(
-          relativePath: { eq: "jessica-ruscello-G8vPQ-XVxxY-unsplash.jpg" }
-        ) {
-          childImageSharp {
-            fluid(quality: 100) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-
         desktopBanner: file(
           relativePath: { eq: "jessica-ruscello-G8vPQ-XVxxY-unsplash.jpg" }
         ) {
@@ -30,37 +20,30 @@ const Hero = () => {
     `
   )
 
-  const source = [
-    mobileBanner.childImageSharp.fluid,
-    {
-      ...desktopBanner.childImageSharp.fluid,
-      media: `(min-width: 750px)`,
-    },
-  ]
+  const source = [desktopBanner.childImageSharp.fluid]
 
   return (
-    <div className="sticky top bg-gray-900">
-      <BackgroundImage
-        style={{ backgroundPosition: "left" }}
-        className="w-full bg-fixed fixed  flex flex-col justify-center md:justify-evenly hero items-start h-full"
-        fluid={source}
-      >
-        <div className="w-full max-w-6xl  fixed">
-          <div className="flex p-6 text-center m-auto flex-col">
-            <h2 className="text-4xl md:text-4xl raleway uppercase underlined text-white">
-              Affordable handmade pottery
-            </h2>{" "}
-            <h1 className="text-xl md:text-3xl chivo-reg text-white my-8">
-              Get 15% off our new collection with code PASTEL01
-            </h1>{" "}
-            <Link to="/shop-home" className="">
-              <Button variant="contained" size="large" color="secondary">
-                Shop Now
-              </Button>
-            </Link>
-          </div>
+    <div className="sticky top bg-gray-900 ">
+      <div className="w-full fixed max-w-6xl relative">
+        {" "}
+        <Img
+          className="w-full bg-fixed absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center md:justify-evenly hero items-start h-screen opacity-75"
+          fluid={source}
+        />
+        <div className="flex justify-center w-full items-center p-6 text-center flex-col absolute top-0 bottom-0 left-0">
+          <h2 className="text-4xl md:text-4xl raleway uppercase underlined text-white">
+            Affordable handmade pottery
+          </h2>{" "}
+          <h1 className="text-xl md:text-3xl chivo-reg text-white my-8">
+            Get 15% off our new collection with code PASTEL01
+          </h1>{" "}
+          <Link to="/shop-home" className="">
+            <Button variant="contained" size="large" color="secondary">
+              Shop Now
+            </Button>
+          </Link>
         </div>
-      </BackgroundImage>
+      </div>
     </div>
   )
 }
