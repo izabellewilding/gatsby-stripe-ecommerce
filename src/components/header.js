19156a046ad40d1bbfdfd18dd64c68800bc7a457
izabelle wilding <izabellewilding@gmail.com>
2020-06-14 15:12:23 +0100

    Header Navigation

--- src/components/header.js
+++ src/components/header.js
@@ -20,35 +20,48 @@ import IconButton from "@material-ui/core/IconButton"
 import MenuIcon from "@material-ui/icons/Menu"
 import MenuItem from "@material-ui/core/MenuItem"
 import Drawer from "@material-ui/core/Drawer"
-import StarBorder from "@material-ui/icons/StarBorder"
 import Collapse from "@material-ui/core/Collapse"
 import StorefrontIcon from "@material-ui/icons/Storefront"
 import HomeIcon from "@material-ui/icons/Home"
-import CloseIcon from "@material-ui/icons/Close"
 import ExpandLess from "@material-ui/icons/ExpandLess"
 import ExpandMore from "@material-ui/icons/ExpandMore"
 import MailIcon from "@material-ui/icons/Mail"
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
-import { Hidden } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   header: {
     background: `url(${CurvyLines})` + theme.palette.primary.light,
     backgroundPosition: "center",
   },
+  menuButton: {
+    display: "block",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  navBar: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
   root: {
     width: "100%",
-    maxWidth: 360,
+    maxWidth: 340,
   },
   nested: {
     paddingLeft: theme.spacing(4),
   },
+
   toolbar: {
     minHeight: 73,
   },
-  iconSection: {
+
+  drawerHeader: {
     width: "100%",
     justifyContent: "space-between",
+    borderBottom: "solid 0.5px white",
+    backgroundColor: theme.palette.primary.light,
   },
 }))
 
@@ -77,29 +90,47 @@ const Header = () => {
   return (
     <>
       <AppBar position="fixed" className={classes.header}>
-        <Toolbar className={classes.iconSection}>
-          <IconButton
-            edge="start"
-            // className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon color="primary" />
-          </IconButton>
-
-          <Img src="../images/name.png" className=" w-32 ml-6 my-2" />
-          <MenuItem>
-            <IconButton aria-label="shopping cart" color="primary">
-              <BadgeLink
-                to="/checkout"
-                badgeContent={ctx.totalItems(ctx.items)}
-                color="secondary"
-              >
-                <ShoppingBasketIcon />
-              </BadgeLink>
+        <Toolbar>
+          <div className="flex justify-start h-full flex-row w-1/3">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon color="primary" />
             </IconButton>
-          </MenuItem>
+            <div className={classes.navBar}>
+              <MenuItem edge="start">
+                <Typography variant="subtitle1">Home</Typography>
+              </MenuItem>
+
+              <MenuItem edge="start">
+                <Typography variant="subtitle1">Shop</Typography>
+              </MenuItem>
+
+              <MenuItem edge="start">
+                <Typography variant="subtitle1">Contact</Typography>
+              </MenuItem>
+            </div>
+          </div>
+          <div className="w-1/3 flex justify-center">
+            <Img src="../images/name.png" className=" w-24 ml-6 my-2" />
+          </div>
+          <div className="w-1/3 flex justify-end">
+            <MenuItem className="w-1/3 flex justify-end">
+              <IconButton aria-label="shopping cart" color="primary">
+                <BadgeLink
+                  to="/checkout"
+                  badgeContent={ctx.totalItems(ctx.items)}
+                  color="secondary"
+                >
+                  <ShoppingBasketIcon />
+                </BadgeLink>
+              </IconButton>
+            </MenuItem>
+          </div>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -111,38 +142,55 @@ const Header = () => {
         // }}
         onClose={handleDrawerToggle}
       >
-        <ListItemLink to="/">
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItemLink>
-        <ListItem button onClick={openNavItem}>
-          <ListItemIcon>
-            <StorefrontIcon />
-          </ListItemIcon>
-          <ListItemText primary="Shop" />
-          {navItemOpen ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={navItemOpen} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemLink to="/shop-home" button className={classes.nested}>
-              <ListItemText primary="Home" />
-            </ListItemLink>
-            <ListItem button className={classes.nested}>
-              <ListItemText primary="Dining" />
-            </ListItem>
-            <ListItem button className={classes.nested}>
-              <ListItemText primary="Plantpots" />
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItemLink to="/contact">
-          <ListItemIcon>
-            <MailIcon />
-          </ListItemIcon>
-          <ListItemText primary="Contact" />
-        </ListItemLink>
+        {" "}
+        <List className={classes.list}>
+          {" "}
+          <ListItem className={classes.drawerHeader}>
+            <Img src="../images/name.png" className=" w-20 ml-6 my-2" />
+
+            <IconButton aria-label="shopping cart" color="primary">
+              <BadgeLink
+                to="/checkout"
+                badgeContent={ctx.totalItems(ctx.items)}
+                color="secondary"
+              >
+                <ShoppingBasketIcon />
+              </BadgeLink>
+            </IconButton>
+          </ListItem>
+          <ListItemLink to="/">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemLink>
+          <ListItem button onClick={openNavItem}>
+            <ListItemIcon>
+              <StorefrontIcon />
+            </ListItemIcon>
+            <ListItemText primary="Shop" />
+            {navItemOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={navItemOpen} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemLink to="/shop-home" button className={classes.nested}>
+                <ListItemText primary="Home" />
+              </ListItemLink>
+              <ListItem button className={classes.nested}>
+                <ListItemText primary="Dining" />
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemText primary="Plantpots" />
+              </ListItem>
+            </List>
+          </Collapse>
+          <ListItemLink to="/contact">
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary="Contact" />
+          </ListItemLink>
+        </List>
       </Drawer>
       <Toolbar />
     </>
