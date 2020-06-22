19f671e51f4d0ae52a9b6933ec0c9dc07bb8f3f9
izabelle wilding <izabellewilding@gmail.com>
2020-06-22 17:24:01 +0100

    Added Stripe Products via Node Script

--- stripe-product.js
+++ stripe-product.js
@@ -2,10 +2,10 @@ var stripe = require("stripe")("sk_test_2PECtktQNqsL5vWpIUuWWWh000wvy5gZuc")
 
 stripe.products.create(
   {
-    name: "Wide Neck Plant Pot",
+    name: "Mug",
     type: "good",
     attributes: ["name"],
-    metadata: { color: "white", width: "25cm", range: "plantpots" },
+    metadata: { color: "Stone", width: "7cm", range: "dining" },
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
   },
--- stripe-sku.js
+++ stripe-sku.js
@@ -2,11 +2,11 @@ var stripe = require("stripe")("sk_test_2PECtktQNqsL5vWpIUuWWWh000wvy5gZuc")
 
 stripe.skus.create(
   {
-    price: 2200,
+    price: 8000,
     currency: "gbp",
-    attributes: { name: "Wide Neck Plant Pot" },
+    attributes: { name: "Mug" },
     inventory: { type: "infinite" },
-    product: "prod_HVraeOm9JFIclu",
+    product: "prod_HVuCCBY4bW8LRO",
   },
   function(err, sku) {
     if (err) return console.log("Error: ", err)
