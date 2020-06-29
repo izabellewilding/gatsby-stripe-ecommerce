836cddd436b19644272008df86bd45595d426736
izabelle wilding <izabellewilding@gmail.com>
2020-06-29 20:49:31 +0100

    Fixed fallback fonts

--- src/components/shop/items-list.js
+++ src/components/shop/items-list.js
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql, StaticQuery } from "gatsby"
 import Item from "./item"
 import { loadStripe } from "@stripe/stripe-js"
+import Slider from "@material-ui/core/Slider"
+import { makeStyles } from "@material-ui/core/styles"
 
 const stripePromise = loadStripe("pk_test_anttTREN4cB8C5RCPRb8vEZL00IHwVyBtk")
 
+const useStyles = makeStyles(theme => ({}))
+
 const Products = () => {
+  const classes = useStyles()
+  const [value, setValue] = useState(0)
   return (
     <StaticQuery
       query={graphql`
@@ -30,10 +36,19 @@ const Products = () => {
       `}
       render={({ skus }) => {
         return (
-          <div className="flex justify-evenly flex-wrap">
-            {skus.edges.map(({ node: sku }) => (
-              <Item key={sku.id} sku={sku} stripePromise={stripePromise} />
-            ))}
+          <div>
+            <div className="flex justify-evenly flex-wrap">
+              {skus.edges.map(({ node: sku }) => (
+                <Item key={sku.id} sku={sku} stripePromise={stripePromise} />
+              ))}
+            </div>
+            <div>
+              <Slider
+                value={value}
+                onChange={(event, newValue) => setValue(newValue)}
+                aria-labelledby="continuous-slider"
+              />
+            </div>
           </div>
         )
       }}
