69db61933d3b977f6f2cbd7bff211b0138074d3a
izabelle wilding <izabellewilding@gmail.com>
2020-07-14 15:11:22 +0100

    Message success page design

--- src/pages/contact-success.js
+++ src/pages/contact-success.js
@@ -1,17 +1,35 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
+
+const ButtonLink = props => {
+  return <Button component={Link} {...props} />
+}
 
 // const useStyles = makeStyles(theme => ({}))
 const Thanks = () => {
   return (
     <Layout>
-      <div>
-        <Typography variant="h1">
-          Thank you! I have received your message and will be in touch
-          shortly...
-        </Typography>
-      </div>
+      <Box className=" justify-center flex" style={{ minHeight: "80vh" }}>
+        <div
+          className="flex flex-col justify-center align-middle m-auto w-4/5 md:w-2/5"
+          color="primary"
+        >
+          <Typography
+            variant="subtitle1"
+            className=" text-gray-800 text-center text-6xl garamond py-12"
+          >
+            Thank you for your message! We'll be in touch as soon as possible
+          </Typography>
+
+          <ButtonLink to="/shop-home" color="secondary" variant="contained">
+            Back to Shop
+          </ButtonLink>
+        </div>
+      </Box>
     </Layout>
   )
 }
